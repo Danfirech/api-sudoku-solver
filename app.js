e3cc2dc9ae212bcf68adb3ad64f3a19d66ba7e32
Dan Possehl <danielbpossehl@gmail.com>
2022-01-07 14:54:19 -0700

    boxes are showing up in HTML

---
+++ app.js
@@ -0,0 +1,14 @@
+const puzzleBoard = document.querySelector("#puzzle");
+const solveButton = document.querySelector("#solve-button");
+
+const squares = 81;
+
+for (let i = 0; i < squares; i++) {
+  const inputElement = document.createElement("input");
+
+  //Created inputs
+  inputElement.setAttribute("type", "number");
+  inputElement.setAttribute("min", "0");
+  inputElement.setAttribute("max", "9");
+  puzzleBoard.appendChild(inputElement);
+}
