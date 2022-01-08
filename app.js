3207d78c5a35e1c8ce82d4dd862212ad3bfc5742
Dan Possehl <danielbpossehl@gmail.com>
2022-01-08 09:09:59 -0700

    array is created when numbers picked

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const puzzleBoard = document.querySelector("#puzzle");
 const solveButton = document.querySelector("#solve-button");
 
 const squares = 81;
+const submission = [];
 
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement("input");
@@ -12,3 +13,17 @@ for (let i = 0; i < squares; i++) {
   inputElement.setAttribute("max", "9");
   puzzleBoard.appendChild(inputElement);
 }
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value) {
+      submission.push(input.value);
+    } else {
+      submission.push(".");
+    }
+  });
+  console.log(submission);
+};
+
+solveButton.addEventListener("click", joinValues);
